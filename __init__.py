5350f6cc786675a323e6914a3891b4b180e37fc8
Lukas Rosendahl <lukas@recordbay.de>
2024-05-22 17:03:14 +0200

    move import

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
 import importlib
 import os
 import json
-from .gcp_storage import NODE_CLASS_MAPPINGS
-from .gcp_storage import NODE_DISPLAY_NAME_MAPPINGS
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
-CONFIG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
 
 def install_gcp_storage():
     print(f"Checking for google-cloud-storage installation..")
@@ -17,11 +14,16 @@ def install_gcp_storage():
         pip.main(["install", "google-cloud-storage"])
 
 install_gcp_storage()
-if not os.path.isfile(CONFIG_FILE):
+if not os.path.isfile(config_file_path):
     print('Creating missing config file..')
     config = {
         "gcp_service_json_path": "xxx"
     }
 
-    with open(CONFIG_FILE, "w") as f:
+    with open(config_file_path, "w") as f:
         json.dump(config, f, indent=2)
+
+
+from .gcp_storage import NODE_CLASS_MAPPINGS
+from .gcp_storage import NODE_DISPLAY_NAME_MAPPINGS
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
