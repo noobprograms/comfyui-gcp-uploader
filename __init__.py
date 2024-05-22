291d00f236b393b08bd2fb8ee43797c87d5d2482
Lukas Rosendahl <lukas@recordbay.de>
2024-05-22 16:51:33 +0200

    mappings

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 import os
 import json
+from .gcp_storage import NODE_CLASS_MAPPINGS
+from .gcp_storage import NODE_DISPLAY_NAME_MAPPINGS
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
 config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
 
