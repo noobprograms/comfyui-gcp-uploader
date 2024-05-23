97cea5ab38d4be47e2d2d66480ad3e0ebce822bd
Lukas Rosendahl <lukas@recordbay.de>
2024-05-23 13:26:54 +0200

    compress level

--- gcp_storage.py
+++ gcp_storage.py
@@ -5,43 +5,13 @@ import json
 import os
 import numpy as np
 
-
 config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json')
-
-def get_api_key():
-    print(f"Checking for Service account json..")
-    try:
-        print(f"Config File Location: {config_file_path}")
-        with open(config_file_path, "r") as f:
-            config = json.load(f)
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= config["gcp_service_json_path"]
-    except:
-        print("Error: Service account json not found")
-
-def save_images(self, images, filename_prefix="ComfyUI"):
-    full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
-    results = list()
-    for (batch_number, image) in enumerate(images):
-        i = 255. * image.cpu().numpy()
-        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
-        metadata = None
-
-        filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
-        file = f"{filename_with_batch_num}.png"
-        img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
-        results.append({
-            "filename": file,
-            "subfolder": subfolder,
-            "type": self.type
-        })
-        counter += 1
-
-    return { "ui": { "images": results } }
      
 class upload_to_gcp_storage:
     def __init__(self):
-        self.type = "output"
         self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.compress_level = 4
         
     @classmethod
     def INPUT_TYPES(s):
@@ -74,10 +44,40 @@ class upload_to_gcp_storage:
         blob = bucket.blob(file)
         blob.upload_from_filename(full_file_path)
 
+def get_api_key():
+    print(f"Checking for Service account json..")
+    try:
+        print(f"Config File Location: {config_file_path}")
+        with open(config_file_path, "r") as f:
+            config = json.load(f)
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= config["gcp_service_json_path"]
+    except:
+        print("Error: Service account json not found")
+
+def save_images(self, images, filename_prefix="ComfyUI"):
+    full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+    results = list()
+    for (batch_number, image) in enumerate(images):
+        i = 255. * image.cpu().numpy()
+        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+        metadata = None
+
+        filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+        file = f"{filename_with_batch_num}.png"
+        img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+        results.append({
+            "filename": file,
+            "subfolder": subfolder,
+            "type": self.type
+        })
+        counter += 1
+
+    return { "ui": { "images": results } }
+
 NODE_CLASS_MAPPINGS = {
-    "GCPStorage": upload_to_gcp_storage,
+    "GCPStorageUpload": upload_to_gcp_storage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "GCPStorage": "GCP Storage Node",
+    "GCPStorageUpload": "GCP Storage Node",
 }
